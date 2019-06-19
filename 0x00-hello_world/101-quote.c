a704721e605951d4b83d3c9f5fc41178f39da296
Sergio Zamudio <szamu5@hotmail.com>
2019-06-19 20:27:01 +0000

    second advance

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - second advance
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, text, 127);
+	return (1);
+}
