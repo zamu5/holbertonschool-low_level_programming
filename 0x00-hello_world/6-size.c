b36c6fa33b93b890302c0b0d8c153534eb40a1bc
Sergio Zamudio <szamu5@hotmail.com>
2019-06-19 18:57:20 +0000

    the size is the same

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %3d byte(s)\n", sizeof(char));
-	printf("Size of a int: %1d byte(s)\n", sizeof(int));
+	printf("Size of a char: %1d byte(s)\n", sizeof(char));
+	printf("Size of an int: %1d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %1d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %2d byte(s)\n", sizeof(float));
+	printf("Size of a float: %1d byte(s)\n", sizeof(float));
 	return (0);
 }
