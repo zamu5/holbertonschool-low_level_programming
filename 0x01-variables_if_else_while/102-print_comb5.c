2dc27bec5fc23f58b26dbfc46c0c3808946fa6d2
Sergio Zamudio <szamu5@hotmail.com>
2019-06-20 19:53:52 +0000

    test

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Patience
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z, c;
+
+	for (x = 48 ; x <= 57 ; x++)
+	{
+		for (y = 48 ; y <= 57 ; y++)
+		{
+			for (z = 48 ; z <= 57 ; z++)
+			{
+				for (c = 48 ; c <= 57 ; c++)
+				{
+					if ((x+y) <= (z+c))
+					{
+					putchar(x);
+					putchar(y);
+					putchar(32);
+					putchar(z);
+                                        putchar(c);
+						if (x != 57 || y != 56 || z != 57 || c != 57)
+						{
+						putchar(44);
+						putchar(32);
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
