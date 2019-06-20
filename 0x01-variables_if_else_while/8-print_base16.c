f909bc11476a578796449638b9be5af543580f1f
Sergio Zamudio <szamu5@hotmail.com>
2019-06-20 16:36:06 +0000

    Now try with hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Hexadecimal
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	char z;
+
+	for (x = 48 ; x <= 57 ; x++)
+	{
+	putchar(x);
+	}
+	for (z = 97 ; z <= 102 ; z++)
+	{
+	putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
