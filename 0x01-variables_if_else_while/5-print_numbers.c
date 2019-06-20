d5ca4c36d3f5695567c25a202708f1f138d4c461
Sergio Zamudio <szamu5@hotmail.com>
2019-06-20 16:20:43 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - lets show the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 48 ; x <= 57 ; x++)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
