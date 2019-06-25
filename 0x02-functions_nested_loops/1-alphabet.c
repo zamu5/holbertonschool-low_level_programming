f7131c754d489595f7c03ca11c5e74a0f53f2504
Sergio Zamudio <szamu5@hotmail.com>
2019-06-25 14:32:42 +0000

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet- check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+int x;
+for (x = 97; x <= 122; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
