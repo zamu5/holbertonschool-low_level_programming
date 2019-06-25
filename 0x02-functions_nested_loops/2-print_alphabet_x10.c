00ecbbe6a1970eb00da9f214ab604b66c6e6b4be
Sergio Zamudio <szamu5@hotmail.com>
2019-06-25 14:39:42 +0000

    ten times the same thing

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,20 @@
-#
+#include "holberton.h"
+/**
+ * print_alphabet_x10- check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+int x, c = 0;
+while (c <= 10)
+{
+for (x = 97; x <= 122; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+c++
+}
+}
