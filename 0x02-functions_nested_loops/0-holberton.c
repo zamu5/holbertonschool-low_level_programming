e71a1f45093b340be1947f208f0e9676642f332a
Sergio Zamudio <szamu5@hotmail.com>
2019-06-25 14:04:13 +0000

    Go Go Go

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
-
-int main(voi)
+#include "holberton.h"
+/**
+ * main - % funtion
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-printf("Holberton\n");
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
