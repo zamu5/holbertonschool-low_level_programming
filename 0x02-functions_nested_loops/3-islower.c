c00098f23f1833ec9b9deda5abe1e4aa08628aca
Sergio Zamudio <szamu5@hotmail.com>
2019-06-25 15:09:36 +0000

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _islower - prints the alphabet in lowercase
+ * Return: 0 if compilation is succesfully
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (0);
+}
+return (1);
+}
