8911dbd68ed106b0fdb0ecec5bd479a271056057
Sergio Zamudio <szamu5@hotmail.com>
2019-06-26 17:43:12 +0000

    advance again

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,10 +7,12 @@
 int main(void)
 {
 	int x, y, z, c;
-	long int  suma3, suma5, sumatotal;
+	int  suma3 = 0, suma5 = 0, sumatotal = 0;
 
 	x = 1024 / 3;
+	printf("%d\n", x);
 	y = 1024 / 5;
+	printf("%d\n", y);
 	for (z = 0 ; z <= x ; z++)
 	{
 		suma3 = suma3 + (z * 3);
@@ -19,6 +21,8 @@ int main(void)
 	{
 		suma5 = suma5 + (c * 5);
 	}
+	printf("%d\n", suma3);
+	printf("%d\n", suma5);
 	sumatotal = suma3 + suma5;
-	printf("%ld\n", sumatotal);
+	printf("%d\n", sumatotal);
 }
