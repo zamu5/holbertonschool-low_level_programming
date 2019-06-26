2a85184867ad0c60afc625760808aac827bf6315
Sergio Zamudio <szamu5@hotmail.com>
2019-06-26 19:14:53 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int ult = 1, penul = 0, sum = 0, cont;
+
+	for (cont = 0 ; cont <= 50 ; cont++)
+	{
+		sum = ult + penul;
+		if (cont != 50)
+		{
+			printf("%ld, ", sum);
+		}
+		else
+		{
+			printf("%ld", sum);
+		}
+		penul = ult;
+		ult = sum;
+	}
+}
