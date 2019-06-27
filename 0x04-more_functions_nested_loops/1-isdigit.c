5358decad580050a8a2ae71534eb405d92188faf
Sergio Zamudio <szamu5@hotmail.com>
2019-06-27 17:18:48 +0000

    betty corrections

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
- * is_digit - Entry point
- *
+ * _isdigit - Entry point
+ *@c: declaration
  * Return: Always 0 (Success)
  */
 int _isdigit(int c)
 {
-        if (c >= 48 && c <= 57)
-        {
-                return (1);
-        }
-        return (0);
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
 }
