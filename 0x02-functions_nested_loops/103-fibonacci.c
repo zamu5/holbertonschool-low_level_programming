5ba07db00b815dac06908baa4eb868ada8c7e0fd
Sergio Zamudio <szamu5@hotmail.com>
2019-06-27 02:11:26 +0000

    4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int ult = 1, penul = 0, sum = 0, even=0;
+
+	for (sum = 0 ; sum < 4000000 ; sum = ult + penul)
+	{
+		if ((sum % 2) == 0)
+		{
+			even = even + sum;
+		}
+		penul = ult;
+		ult = sum;
+	}
+	printf("%ld", even);
+	printf("\n");
+	return (0);
+}
