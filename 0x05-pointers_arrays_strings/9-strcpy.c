c7d44f2ad543c277ec387c4e13a6309f5ca14701
Sergio Zamudio <szamu5@hotmail.com>
2019-07-02 18:02:27 +0000

    unclair

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 /**
  * _strcpy - asign value to pointer.
- *
+ *@dest: declaration
+ *@src: declaration
  * Return: Always 0.
  */
 char *_strcpy(char *dest, char *src)
@@ -10,7 +11,7 @@ char *_strcpy(char *dest, char *src)
 
 	for (x = 0 ; src[x] != '\0' ; x++)
 	{
-		dest[x] = src [x];
+		dest[x] = src[x];
 	}
 	dest[x] = '\0';
 	return (dest);
