f4d9e22bc5036507b3554bbd4d68e05966c0bccb
Sergio Zamudio <szamu5@hotmail.com>
2019-07-02 13:47:25 +0000

    pair to pair

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,7 @@ void puts2(char *str)
 {
 	int x, y = 0;
 
-	for (x = 0 ; str[x] != '\0' && str[x-y] != '\0' ; x = x + 2)
+	for (x = 0 ; str[x] != '\0' && str[x - y] != '\0' ; x = x + 2)
 	{
 		_putchar(str[x]);
 		y = 1;
