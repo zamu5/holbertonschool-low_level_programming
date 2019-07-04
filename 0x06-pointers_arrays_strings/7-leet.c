6fcf8754a8fc75c965bd7482ae9437f54fba2106
Sergio Zamudio <szamu5@hotmail.com>
2019-07-04 15:51:01 -0500

    parallel arrays

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * leet - check the code for Holberton School students.
+ *@s: declaration
+ * Return: Always 0.
+ */
+char *leet(char *s)
+{
+int x, y;
+
+int datos[] = {65, 97, 69, 101, 79, 111, 84, 116, 76, 108};
+int rempl[] = {52, 52, 51, 51, 48, 48, 55, 55, 49, 49};
+
+	for (x = 0 ; s[x] != '\0' ; x++)
+	{
+		for (y = 0; y <= 10 ; y++)
+		{
+			if(s[x] == datos[y])
+			{
+				s[x] = rempl[y];
+			}
+		}
+	}
+	return (s);
+}
