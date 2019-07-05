72baddf7e1ec0134d22c11d5d686d6cfe25965cd
Sergio Zamudio <szamu5@hotmail.com>
2019-07-04 22:04:13 -0500

    you have to check with betty

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -16,25 +16,24 @@ void print_number(int n)
 	n = n > 0 ? n : n * -1;
 	z = n;
 	while (n > 9)
-	{
-		n = n /10;
-		cont++;
-	}
-	for (x = 1 ; x < cont ; x++)
 		{
-			c = c * 10;
+		n = n / 10;
+		cont++;
 		}
+	for (x = 1 ; x < cont ; x++)
+	{
+		c = c * 10;
+	}
 	for (x = 0 ; x < cont ; x++)
 	{
 		if (z > 9)
+			_putchar((z / c) + '0');
+		else
 		{
-			_putchar((z/c) + '0');
-		}
-		else {
-			_putchar((z%10) + '0');
+			_putchar((z % 10) + '0');
 		}
 		z = z % c;
-		c = c /10;
+		c = c / 10;
 	}
 
 
