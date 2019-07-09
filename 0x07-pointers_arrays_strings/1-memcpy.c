446fd239bcac5557442d9d9ab53ed6abb5a2fc31
Sergio Zamudio <szamu5@hotmail.com>
2019-07-08 20:15:45 -0500

    fix betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
+ * _memcpy - Write a function that copies memory area
+ * @src: declaration
+ * @dest: declaration
+ * @n: declaration
  * Return: Nothing.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * _strchr - Write a function that locates a character in a string
+ * @s: delcaration
+ * @c: declaration
  * Return: Always 0.
  */
 char *_strchr(char *s, char c)
 {
 	int x = 0;
 
-	while( s[x] != c)
+	while (s[x] != c)
 	{
 		if (s[x] == '\0')
 			return (0);
