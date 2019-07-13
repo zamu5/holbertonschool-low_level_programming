27391259703424c5ba1a0186abcc5bea8649159e
Sergio Zamudio <szamu5@hotmail.com>
2019-07-13 13:47:42 -0500

    correct betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: declaration
  * Return: Always 0.
  */
-int main (int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
