15eedef5ef41f67b51a7a41bc303bc51b30999eb
Sergio Zamudio <szamu5@hotmail.com>
2019-07-17 13:56:16 -0500

    create free

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -12,26 +12,22 @@ char **strtow(char *str)
 {
 	char **m = NULL;
 	char *p = NULL;
-	int x, y, c, z = 0, len, words = 0, start = 0;
-	if(str == 0)
-		return (NULL);
+	int x, y, c, n, z = 0, len, words = 0;
+
 	for (x = 0; *(str + x) != '\0'; x++)
-	{
 		if (*(str + x) != 32 && (*(str + (x + 1)) == 32 ||
 					 *(str + (x + 1)) == 0))
 			words++;
-		if (*(str + x) == 32 && words == 0)
-			start++;
-	}
-	if (words == 0)
+	if (words == 0 || str == 0)
 		return (NULL);
 	m = (char **) malloc(sizeof(char *) * (words + 1));
 	if (m != 0)
 	{
-		z = start;
 		for (x = 0; x < words; x++)
 		{
 			len = 0;
+			for (; str[z] == 32; z++)
+				;
 			for (y = z; (*(str + y)) != 32; y++, z++)
 				len++;
 			p = (char *) malloc(sizeof(char) * (len + 1));
@@ -43,15 +39,15 @@ char **strtow(char *str)
 					*(*(m + x) + y) = *(str + (c - len));
 			}
 			else
+			{
+				for (n = 0; n < x; n++)
+					free(*(m + n));
+				free(m);
 				return (NULL);
-			for (; str[z] == 32; z++)
-				;
+			}
 		}
 		return (m);
 	}
 	else
-	{
-		free(m);
 		return (NULL);
-	}
 }
