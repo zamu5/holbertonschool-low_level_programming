91944aa0f29320e6dd4329569de109507ce9ae37
Sergio Zamudio <szamu5@hotmail.com>
2019-07-18 14:13:57 -0500

    array with two integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	p = malloc(sizeof(int) * (max - min + 1));
+	p = malloc(sizeof(int) * (max - min));
 	if (p == 0)
 		return (NULL);
 	s = (int *)p;
