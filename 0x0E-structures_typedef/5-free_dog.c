330238509a50f2a213ab48d4d6069dacf2cfb1a5
Sergio Zamudio <szamu5@hotmail.com>
2019-07-22 14:12:17 -0500

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dog
+ * @d: dog
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
