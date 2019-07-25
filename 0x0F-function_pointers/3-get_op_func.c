b43255e464fd377c2dc47a97b15fa04915951d9f
Sergio Zamudio <szamu5@hotmail.com>
2019-07-24 20:47:29 -0500

    fixes

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -15,9 +15,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 		};
-	int i = 5;
+	int i = 0;
 
 	while (*ops[i].op != *(s + 0) && s[1] == '\0')
-		i--;
+	{
+		if (s[1] != '\0')
+			return (NULL);
+		i++;
+	}
 	return (*ops[i].f);
 }
