6ebc344af135e6308a16597fd8c8688d76569b59
Sergio Zamudio <szamu5@hotmail.com>
2019-07-27 14:47:07 -0500

    Fix: if n = NULL return 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,6 +9,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_list arguments;
 	unsigned int sum = 0, x;
 
+	if (n == 0)
+		return (0);
 	va_start(arguments, n);
 	for (x = 0; x < n; x++)
 		sum = sum + va_arg(arguments, int);
