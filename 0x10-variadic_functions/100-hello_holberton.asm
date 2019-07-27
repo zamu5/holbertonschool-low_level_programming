77269808f67e047291d609f50dc5ec26029d2479
Sergio Zamudio <szamu5@hotmail.com>
2019-07-27 15:19:46 -0500

    Fix: syntax

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,8 +1,8 @@
-	section .data
+section .data
 	greeting db "Hello, Holberton", 10
-	section .text
-	global _start
-_start:
+section .text
+	global main
+main:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, greeting
