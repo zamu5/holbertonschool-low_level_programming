c9fd86a55ea34dda03cb5459cf97c09c1c65e637
Sergio Zamudio <szamu5@hotmail.com>
2019-08-01 21:11:14 -0500

    fix: check betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-void first_this (void) __attribute__ ((constructor));
+void first_this(void) __attribute__ ((constructor));
 /**
  * first_this - print first this
  *
  * Return: Nothing
  */
-void first_this (void)
+void first_this(void)
 {
-    printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
