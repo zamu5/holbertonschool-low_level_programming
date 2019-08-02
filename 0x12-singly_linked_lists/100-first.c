4fc63e64aa2121b0d5f99bbcb3c7678d2ca15afe
Sergio Zamudio <szamu5@hotmail.com>
2019-08-01 21:07:42 -0500

    feature: print before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void first_this (void) __attribute__ ((constructor));
+/**
+ * first_this - print first this
+ *
+ * Return: Nothing
+ */
+void first_this (void)
+{
+    printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
