6ad3d013ebb991affbca5dd31a008447554e01f6
Sergio Zamudio <szamu5@hotmail.com>
2019-08-08 16:52:32 -0500

    [FIX] is weird

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * flip_bits - how many bits would be fliped to convert a number to other one
  * @n: number 1
@@ -7,15 +8,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int data1, data2, c;
+	unsigned long int data = 0, c;
 	int cont = 0, i = 0;
 
+	data = n ^ m;
 	while (31 - i >= 0)
 	{
-		data1 = 1 << (31 - i) & n;
-		data2 = 1 << (31 - i) & m;
-
-		c = data1 ^ data2;
+		c = data & 1 << (31 - i);
 		if (c)
 			cont++;
 		i++;
