8909f5d84fa5fad356b3be50041f6bbff9ecd15f
Sergio Zamudio <szamu5@hotmail.com>
2019-08-08 14:01:17 -0500

    [FEATURE] get a specific bit of a int

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * get_bit - found a specific bit
+ * @n: the data number
+ * @index: the bit to found
+ * Return: e value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1, c;
+
+	c = mask << index & n;
+	if (c)
+		return (1);
+	else
+		return (0);
+	return (0);
+}
