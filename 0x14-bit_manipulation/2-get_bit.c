30051896fcf2068ad7f54dc53670f9b68011330f
Sergio Zamudio <szamu5@hotmail.com>
2019-08-08 14:18:14 -0500

    [FIX] if index > 63 return -1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,10 +9,12 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask = 1, c;
 
+	if (index > 63)
+		return (-1);
 	c = mask << index & n;
 	if (c)
 		return (1);
 	else
 		return (0);
-	return (0);
+	return (-1);
 }
