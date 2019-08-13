9dddef563935bcc11232ec78cd5773736ea9eb35
Sergio Zamudio <szamu5@hotmail.com>
2019-08-13 18:47:31 -0500

    [FIX] add conditionf if cont != wf

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (nf);
 	if (text_content != NULL)
 		wf = write(nf, text_content, cont);
-	if (wf == -1)
+	if (wf == -1 || wf != cont)
 		return (wf);
 	return (1);
 }
