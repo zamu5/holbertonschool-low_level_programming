2e25a633c253c26407cdd87d6942985bae271fee
Sergio Zamudio <szamu5@hotmail.com>
2019-08-13 18:52:27 -0500

    [FIX] add conditionf if cont != wf

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	while (text_content[cont] != '\0')
 		cont++;
-	nf = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	nf = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (nf == -1)
 		return (-1);
 	if (text_content != NULL)
