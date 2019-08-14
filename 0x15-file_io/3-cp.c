ce2f24e3875e16aa2a5ba886d401e2fef718191c
Sergio Zamudio <szamu5@hotmail.com>
2019-08-13 22:37:34 -0500

    [FIX] NULL

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,72 +1,49 @@
-#include <stdio.h>
 #include "holberton.h"
+#include <stdio.h>
 /**
- * printerror - printerror
- * @n: error
- * @p: string
- * Return: nothing
+ * main - copies a file
+ *
+ * @argc: argument cont
+ * @argv: arguments
+ * Return: Returns 97 for syntax error, 98 for read error, 99 write, 100 close
  */
-void printerror(int n, char *p)
+int main(int argc, char *argv[])
 {
-	if (n == 97)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	if (n == 98)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", p);
-		exit(98);
-	}
-	if (n == 99)
-	{
-		dprintf(STDERR_FILENO, "Erro\r: Can't write to file %s\n", p);
-		exit(99);
-	}
-}
-/**
- * main - copy a file
- * @ac: the number of arguments
- * @av: arguments
- * Return: nothing
- */
-int main(int ac, char *av[])
-{
-	int ff, ft, rff, ctf, cf;
+	int ff, ft, readval, retval;
 	char buf[1024];
 
-	if (ac != 3)
-		printerror(97, av[1]);
-	if (av[1] == NULL)
-		printerror(98, av[1]);
-	if (av[2] == NULL)
-		printerror(99, av[2]);
-	ff = open(av[1], O_RDONLY);
-	rff = read(ff, buf, 1024);
-	if (rff == -1)
-		printerror(98, av[1]);
-	ft = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	while (rff != 0)
+	retval = 1;
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
+	if (argv[1] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
+	if (argv[2] == NULL)
+		dprintf(STDERR_FILENO, "Erro\r: Can't write to file %s\n", argv[2]),
+			exit(99);
+	ff = open(argv[1], O_RDONLY);
+	ft = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	readval = read(ff, buf, 1024);
+	if (readval == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
+	while (readval != 0)
 	{
-		ctf = write(ft, buf, rff);
-		if (ctf == -1)
-			printerror(99, av[2]);
-		rff = read(ff, buf, 1024);
-		if (rff == -1)
-			printerror(98, av[1]);
+		retval = write(ft, buf, readval);
+		if (retval == -1 || retval != readval)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		readval = read(ff, buf, 1024);
+		if (readval == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+				exit(98);
 	}
-	cf = close(ff);
-	if (cf == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff);
-		exit(100);
-	}
-	cf = close(ft);
-	if (cf == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ft);
-		exit(100);
-	}
+	retval = close(ff);
+	if (retval == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff), exit(100);
+	retval = close(ft);
+	if (retval == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ft), exit(100);
 	return (0);
-
 }
