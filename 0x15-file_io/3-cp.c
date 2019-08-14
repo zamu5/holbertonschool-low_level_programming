dc4fcbc0b5aa1566a8017e8be1700a4b594bdf10
Sergio Zamudio <szamu5@hotmail.com>
2019-08-13 22:48:46 -0500

    [FIX] NULL

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,8 +33,7 @@ int main(int ac, char *av[])
 	if (ac != 3)
 		printerror(97, av[1]);
 	if (av[1] == NULL)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
-			exit(98);
+		printerror(98, av[1]);
 	if (av[2] == NULL)
 		dprintf(STDERR_FILENO, "Erro\r: Can't write to file %s\n", av[2]),
 			exit(99);
