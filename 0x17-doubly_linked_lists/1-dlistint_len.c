7ad3dc212d58dff8a3df3c0d451ee15c20ba7a04
Sergio Zamudio <szamu5@hotmail.com>
2019-09-14 12:12:08 -0500

    FEATURE: count the nodes of a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - count the nodes in a list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	if (h == NULL)
+		return (0);
+	while ((*h).prev != NULL)
+		h = (*h).prev;
+	while (h != NULL)
+	{
+		h = (*h).next;
+		i++;
+	}
+	return (i);
+}
