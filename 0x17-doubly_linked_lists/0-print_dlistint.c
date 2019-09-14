2676d715be1607e6a8c2be1810c30cc4c1111cdf
Sergio Zamudio <szamu5@hotmail.com>
2019-09-14 12:05:46 -0500

    FEATURE: print a double list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - print a list
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	while ((*h).prev != NULL)
+		h = (*h).prev;
+	while (h != NULL)
+	{
+		printf("%d\n", (*h).n);
+		h = (*h).next;
+		i++;
+	}
+	return (i);
+}
