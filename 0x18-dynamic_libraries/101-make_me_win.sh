39e8239458703451a45069660b292130d6a6c527
Sergio Zamudio <szamu5@hotmail.com>
2019-09-16 17:02:06 -0500

    FIX

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/zamu5/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/libtools.so 
-export LD_PRELOAD=./libtools.so
+wget -P /tmp/ https://raw.github.com/zamu5/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/libtools.so 
+export LD_PRELOAD=/tmp/libtools.so
