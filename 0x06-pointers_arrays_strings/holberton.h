febb55494703d78a2f37626505a093b03f87f3d6
Sergio Zamudio <szamu5@hotmail.com>
2019-09-24 10:11:32 -0500

    Revert "Revert "Revert "read"""
    This reverts commit 8a518ca1c07a73535b897a9c10e65f45dfb87e7e.

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -9,5 +9,3 @@ char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
 int _putchar(char c);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
--- 0x18-dynamic_libraries/rand.c
+++
@@ -1,9 +0,0 @@
-int rand(void)
-{
-	static int i = -1;
-
-	int ran[6] = {8, 8, 7, 9, 23, 74};
-
-	i++;
-	return(ran[i]);
-}
