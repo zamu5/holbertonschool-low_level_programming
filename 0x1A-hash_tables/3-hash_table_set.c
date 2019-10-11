d1c34622ed44058befd004e81ecd69861aedbd68
Sergio Zamudio <szamu5@hotmail.com>
2019-10-11 12:35:01 -0500

    FIX: correct when update a key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (new == NULL)
 			return (0);
 		(*new).key = strdup(key);
+		if (!(*new).key)
+			return (0);
 		(*new).value = strdup(value);
+		if (!(*new).value)
+			return (0);
 		(*new).next = (*ht).array[idx];
 	}
 	else
