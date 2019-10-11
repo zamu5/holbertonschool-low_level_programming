7b8765deb23b506a0dcedebebfa9569fa3be9c7a
Sergio Zamudio <szamu5@hotmail.com>
2019-10-11 09:58:58 -0500

    hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	 hash_table_t *new;
+
+	 if(size == 0)
+		 return (NULL);
+	 new = malloc(sizeof(hash_table_t));
+	 if (!new)
+		 return (NULL);
+	 (*new).size = size;
+	 (*new).array = malloc(sizeof(hash_node_t *));
+	 if (!(*new).array)
+		 return (NULL);
+	 return (new);
+}
