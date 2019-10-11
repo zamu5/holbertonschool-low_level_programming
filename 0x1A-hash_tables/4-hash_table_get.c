13acdb102b9d9418df6ea5e27c8f1882150b2523
Sergio Zamudio <szamu5@hotmail.com>
2019-10-11 14:14:08 -0500

    FEATURE: look for the value of a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - look for the value of a specific key
+ * @ht: hashtable
+ * @key: key
+ * Return: value of the key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *check;
+
+	idx = key_index((unsigned char *)key, (*ht).size);
+
+	check = (*ht).array[idx];
+	if (check)
+	{
+		while (check && strcmp((*check).key, key) != 0)
+			check = (*check).next;
+		if (check)
+			return ((*check).value);
+	}
+	return (NULL);
+}
