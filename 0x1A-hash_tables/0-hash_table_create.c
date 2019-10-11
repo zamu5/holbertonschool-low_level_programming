003236da607f14d3d806c2adb16af0a13a86793d
Sergio Zamudio <szamu5@hotmail.com>
2019-10-11 10:10:57 -0500

    FIX: check betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,16 +1,21 @@
 #include "hash_tables.h"
+/**
+ * hash_table_create - create a hash table
+ * @size: the size of the array
+ * Return: A pointer to the hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	 hash_table_t *new;
+	hash_table_t *new;
 
-	 if(size == 0)
-		 return (NULL);
-	 new = malloc(sizeof(hash_table_t));
-	 if (!new)
-		 return (NULL);
-	 (*new).size = size;
-	 (*new).array = malloc(sizeof(hash_node_t *));
-	 if (!(*new).array)
-		 return (NULL);
-	 return (new);
+	if (size == 0)
+		return (NULL);
+	new = malloc(sizeof(hash_table_t));
+	if (!new)
+		return (NULL);
+	(*new).size = size;
+	(*new).array = malloc(sizeof(hash_node_t *) * (*new).size);
+	if (!(*new).array)
+		return (NULL);
+	return (new);
 }
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,10 @@
 #include "hash_tables.h"
+/**
+ * key_index- lookfor the index of a key
+ * @key: the key to looking for
+ * @size: the size of the array
+ * Return: index
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
