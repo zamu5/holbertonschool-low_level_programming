ae0046c4d9b07b129eedc0d24cd868b5b1631064
Sergio Zamudio <szamu5@hotmail.com>
2019-10-11 11:58:58 -0500

    FIX: replace the node at ht

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,6 +21,7 @@ int hash_table_set(hash_table_t *ht,const char *key,const char *value)
 		(*new).key = strdup(key);
 		(*new).value = strdup(value);
 		(*new).next = NULL;
+		(*ht).array[idx] = new;
 	}
 	else
 	{
@@ -31,6 +32,7 @@ int hash_table_set(hash_table_t *ht,const char *key,const char *value)
 		(*new).key = strdup(key);
 		(*new).value = strdup(value);
 		(*new).next = old;
+		(*ht).array[idx] = new;
 	}
 	return (1);
 }
