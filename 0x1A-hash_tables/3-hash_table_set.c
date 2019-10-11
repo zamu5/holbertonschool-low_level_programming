c7bd106e3bf39958e3e5314031762eb08cb726ae
Sergio Zamudio <szamu5@hotmail.com>
2019-10-11 13:34:31 -0500

    fix: ht-> next

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,47 +5,44 @@
  * @key: key
  * @value: value
  * Return: 1 in succes 0 in faik
-*/
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx;
 	hash_node_t *new;
-	char *k, *v;
+	unsigned long int idx;
 
 	if (!ht || !key || !value || strlen(key) == 0)
 		return (0);
-	k = strdup(key);
-	v = strdup(value);
-	if (!k || !v)
-		return (0);
 	idx = key_index((unsigned char *)key, 1024);
-	if (!(*ht).array[idx])
+	new = (*ht).array[idx];
+	if (new)
 	{
-		new  = malloc(sizeof(hash_node_t));
-		if (new == NULL)
-			return (0);
-		(*new).key = k;
-		(*new).value = v;
-		(*new).next = (*ht).array[idx];
-	}
-	else
-	{
-		new = (*ht).array[idx];
 		while (new && strcmp((*new).key, key) != 0)
 			new = (*new).next;
 		if (new && strcmp((*new).key, key) == 0)
 		{
 			free((*new).value);
-			(*new).value = v;
+			(*new).value = strdup(value);
+			if (!(*new).value)
+				return (0);
 			return (1);
 		}
-		new  = malloc(sizeof(hash_node_t));
-		if (new == NULL)
-			return (0);
-		(*new).key = k;
-		(*new).value = v;
-		(*new).next = (*ht).array[idx];
 	}
+	new = malloc(sizeof(hash_node_t));
+	(*new).key = strdup(key);
+	if (!(*new).key)
+	{
+		free(new);
+		return (0);
+	}
+	(*new).value = strdup(value);
+	if (!(*new).value)
+	{
+		free(new);
+		free((*new).key);
+		return (0);
+	}
+	(*new).next = (*ht).array[idx];
 	(*ht).array[idx] = new;
 	return (1);
 }
