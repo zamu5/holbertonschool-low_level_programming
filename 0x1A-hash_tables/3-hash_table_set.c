e126bc4da190e05fb16212438e7675e8ca7758d2
Sergio Zamudio <szamu5@hotmail.com>
2019-10-11 12:46:55 -0500

    FIX: correct when update a key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,52 +1,51 @@
 #include "hash_tables.h"
+#include <string.h>
+
 /**
- * hash_table_set - createa new node at the table
- * @ht: hashtable
- * @key: key
- * @value: value
- * Return: 1 in succes 0 in faik
+ * hash_table_set - sets a value to a key in a hash_table_t
+ *
+ * @ht: hash_table_t
+ * @key: key for bucket
+ * @value: value for bucket
+ * Return: Returns 1 on success, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx;
-	hash_node_t *new, *old;
-	char *k, *v;
+	unsigned long int hsh;
+	hash_node_t *node, *walk;
 
 	if (!ht || !key || !value || strlen(key) == 0)
 		return (0);
-	k = strdup(key);
-	v = strdup(value);
-	if (!k || !v)
-		return (0);
-	idx = key_index((unsigned char *)key, 1024);
-	if (!(*ht).array[idx])
+	hsh = key_index((unsigned char *)key, ht->size);
+	if (ht->array[hsh])
 	{
-		new  = malloc(sizeof(hash_node_t));
-		if (new == NULL)
-			return (0);
-		(*new).key = k;
-		(*new).value = v;
-		(*new).next = (*ht).array[idx];
-	}
-	else
-	{
-		new = (*ht).array[idx];
-		while (new && strcmp((*new).key, key) != 0)
-			new = (*new).next;
-		if (new && strcmp((*new).key, key) == 0)
+		walk = ht->array[hsh];
+		while (walk && strcmp(walk->key, key) != 0)
+			walk = walk->next;
+		if (walk && strcmp(walk->key, key) == 0)
 		{
-			free((*new).value);
-			(*new).value = v;
+			free(walk->value);
+			walk->value = strdup(value);
 			return (1);
 		}
-		old = new;
-		new  = malloc(sizeof(hash_node_t));
-		if (new == NULL)
-			return (0);
-		(*new).key = k;
-		(*new).value = v;
-		(*new).next = old;
 	}
-	(*ht).array[idx] = new;
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+	node->next = ht->array[hsh];
+	ht->array[hsh] = node;
 	return (1);
 }
