6cdcc05250a2e7285b52acdb673244a8be629603
Sergio Zamudio <szamu5@hotmail.com>
2019-10-28 15:25:56 -0500

    [FEATURE]: Adding a new sort function (counting) New File: 102-O and 102-counting_sort Modified: sort.h

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -27,4 +27,5 @@ void _qsort(int *a, int low, int high, int size);
 void shell_sort(int *array, size_t size);
 void _ssort(int *a, int size, int n);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 #endif
