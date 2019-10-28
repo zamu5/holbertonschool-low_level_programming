ce6488ff4cda1a5badf159f932c8dbcf3ece037b
Sergio Zamudio <szamu5@hotmail.com>
2019-10-28 16:08:14 -0500

    [Feature]: adding a function that executes counting sort algorithm New file: 102-O and 102-counting_sort.c Modified: sort.h

--- 0x1B-sorting_algorithms/102-counting_sort.c
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -9,23 +9,30 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int n = 0, i;
+	int n, i;
 	int *buff, *a;
 
-	for (i = 0; i < (int)size; i++)
+	if (size < 2)
+		return;
+
+	for (n = i = 0; i < (int)size; i++)
 		if (array[i] > n)
 			n = array[i];
+
 	buff = malloc(sizeof(int) * (n + 1));
 	if (!buff)
 		return;
+
 	for (i = 0; i <= n; i++)
 		buff[i] = 0;
 	for (i = 0; i < (int)size; i++)
 		buff[array[i]] += 1;
 	for (i = 1; i <= n; i++)
 		buff[i] += buff[i - 1];
+
 	print_array(buff, (n + 1));
-	a = malloc(sizeof(int) * size);
+	a = malloc(sizeof(int) * (size + 1));
+
 	if (!a)
 	{
 		free(buff);
@@ -33,11 +40,13 @@ void counting_sort(int *array, size_t size)
 	}
 	for (i = 0; i < (int)size; i++)
 	{
-		a[buff[array[i]]] = array[i];
+		a[buff[array[i]] - 1] = array[i];
 		buff[array[i]] -= 1;
 	}
-	for (i = 1; i < (int)size; i++)
-		array[i - 1] = a[i];
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = a[i];
+
 	free(buff);
 	free(a);
 }
