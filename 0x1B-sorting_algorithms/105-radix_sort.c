158a596ea3023b233500c4b7aea0a47701d6cd8f
Sergio Zamudio <szamu5@hotmail.com>
2019-10-29 18:58:37 -0500

    [FIX]: print each time loop Modified: 105-radix_sort.c

--- 0x1B-sorting_algorithms/105-radix_sort.c
+++ 0x1B-sorting_algorithms/105-radix_sort.c
@@ -9,32 +9,28 @@
 void radix_sort(int *array, size_t size)
 {
 	int flag = 1, n = 10;
-	size_t i;
+	size_t i, f;
 
 	while (flag)
 	{
 		flag = 0;
-		for (i = 1; i <  size; i++)
+		for (i = 1, f = 1; i <  size; i++, f++)
 		{
 			if ((array[i - 1] % (n * 10)) / ((n * 10) / 10) > 0)
 				flag = 1;
-			else
-				break;
-
 			if (((array[i - 1] % n) / (n / 10)) > ((array[i] % n) / (n / 10)))
 			{
 				array[i - 1] = array[i - 1] + array[i];
 				array[i] = array[i - 1] - array[i];
 				array[i - 1] = array[i - 1] - array[i];
-				flag = 1;
 				if ((i - 1) > 0)
 					i = i - 2;
 			}
 		}
+		print_array(array, size);
 		if (flag)
 		{
 			n = n * 10;
 		}
-		print_array(array, size);
 	}
 }
