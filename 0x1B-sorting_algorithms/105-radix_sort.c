a5a13fd18b70adf6d51e6a47ff7b97da34e0405a
Sergio Zamudio <szamu5@hotmail.com>
2019-10-29 15:15:32 -0500

    [Feature]: adding a function that executes the radix algorithm New file: 105-radix_sort.c and 105-O Modified: sort.h

---
+++ 0x1B-sorting_algorithms/105-radix_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * radix_sort - sorts an array of integers in ascending
+ * order using the Radix sort algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int flag = 1, change, n = 10;
+	size_t i;
+
+	while (flag)
+	{
+		change = 0;
+		for (i = 1; i <  size; i++)
+		{
+here:
+			if ((array[i - 1] % n) > (array[i] % n))
+			{
+				array[i - 1] = array[i - 1] + array[i];
+				array[i] = array[i - 1] - array[i];
+				array[i - 1] = array[i - 1] - array[i];
+				change = 1;
+				if ((i - 1) > 0)
+				{
+					i--, goto here;
+				}
+			}
+		}
+		if (change)
+			print_array(array, size);
+		n = n * 10;
+		if (!change)
+			flag = 0;
+	}
+}
