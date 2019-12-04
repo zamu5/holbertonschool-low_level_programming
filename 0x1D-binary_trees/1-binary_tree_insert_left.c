58564b5ccae2dbfcacd6ebcdd95c7cf6aa1613f2
Sergio Zamudio <szamu5@hotmail.com>
2019-12-04 13:08:49 -0500

    [FIX]: Return NULL when parent is NULL too, Modified: 1-binary_tree_insert_left.c

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -10,6 +10,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (!parent)
+		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
 		return (new);
