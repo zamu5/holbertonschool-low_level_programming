fde4d50c11c40d14911d77adbaab156280aacf82
Sergio Zamudio <szamu5@hotmail.com>
2019-12-04 13:01:35 -0500

    [FIX]: check betty style Modified:  0-binary_tree_node.c

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -10,11 +10,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
-	if(!new)
-		return(new);
+	if (!new)
+		return (new);
 	new->parent = parent;
 	new->n = value;
 	new->left = NULL;
 	new->right = NULL;
-	return(new);
+	return (new);
 }
