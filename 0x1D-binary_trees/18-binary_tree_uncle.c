2cbcd336c93e6915d94dc14e936ac109064de253
Sergio Zamudio <szamu5@hotmail.com>
2019-12-05 16:37:25 -0500

    [FIX]: When parent is NULL Modified: 18-binary_tree_uncle.c

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -7,7 +7,7 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || !node->parent->parent || !node->parent)
+	if (!node || !node->parent || !node->parent->parent )
 		return (NULL);
 	if (node->parent->parent->left && node->parent->parent->left != node->parent)
 		return (node->parent->parent->left);
