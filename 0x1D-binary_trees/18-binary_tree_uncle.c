9aeaa93741306832923aa4af28a9e68e26b59174
Sergio Zamudio <szamu5@hotmail.com>
2019-12-05 16:30:08 -0500

    [FEATURE]: finds the uncle of a node New file:   18-binary_tree_uncle.c Modified:   binary_trees.h

---
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Entry point
+ * @node: node
+ * Return: Always 0 (Success)
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent->parent)
+		return (NULL);
+	if (node->parent->parent->left && node->parent->parent->left != node->parent)
+		return (node->parent->parent->left);
+	if (node->parent->parent->right &&
+	    node->parent->parent->right != node->parent)
+		return (node->parent->parent->right);
+	return (NULL);
+}
