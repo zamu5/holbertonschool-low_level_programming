829dd361c4af3ad8cac1720ef7d89f373b6f24ce
Sergio Zamudio <szamu5@hotmail.com>
2020-01-15 14:56:53 -0500

    FEATURE: update prototipes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,8 @@
 #ifndef AL
 #define AL
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
